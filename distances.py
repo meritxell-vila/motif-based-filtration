cbf03ed264e6cebc3101551a4800b03be592a2e0
meritxell-vila <meritxellvila@josoc.cat>
2025-02-10 23:43:25 +0100

    get_all_diagrams fixed

--- distances.py
+++ distances.py
@@ -1,8 +1,19 @@
 import networkx as nx
 import numpy as np
 
-
 def generate_shortest_path_distance_matrix(G: nx.Graph):
+    # Ensure node labels are sequential from 0 to len(G)-1
+    nodes = list(G.nodes)  
+    node_index = {node: i for i, node in enumerate(nodes)} 
+
+    distance_matrix = np.ones((len(nodes), len(nodes))) * np.inf
+    shortest_paths = dict(nx.all_pairs_shortest_path_length(G))
+    for node in nodes:
+        for target_node, distance in shortest_paths.get(node, {}).items():
+            distance_matrix[node_index[node], node_index[target_node]] = distance
+    return distance_matrix
+
+def generate_shortest_path_distance_matrix_old(G: nx.Graph):
     shortest_paths = nx.all_pairs_shortest_path_length(G)
     nodes = G.nodes
     distance_matrix = np.ones((len(nodes), len(nodes))) * np.inf
